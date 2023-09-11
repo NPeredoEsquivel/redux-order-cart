90696816df4ca737e6c7033a8d74737488d04950
Nicolas Peredo <nicolas.peredo@globant.com>
2023-09-11 07:02:07 -0300

    Adding timemout to a UI action

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products/Products";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCartData, sendCartData } from "./store/cartSlice/cartActions";
+import { toggleNotificationUI } from "./store/uiSlice/uiActions";
 
 function App() {
   const { cartVisible, notification } = useSelector((state) => state.ui);
   const { items, changed } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
-  console.log("notification", notification);
+
   useEffect(() => {
     dispatch(fetchCartData());
   }, [dispatch]);
@@ -18,6 +19,7 @@ function App() {
   useEffect(() => {
     if (items.length > 0 && changed) {
       dispatch(sendCartData(items));
+      dispatch(toggleNotificationUI());
     }
   }, [items, changed, dispatch]);
   return (
